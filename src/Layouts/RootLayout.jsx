8dff343bac0ca624eef535b2c3c0bb63ed8465db
samibyte <samibyte.web@gmail.com>
2025-10-07 21:14:48 +0600

    Initialize structure and add assets

---
+++ src/Layouts/RootLayout.jsx
@@ -0,0 +1,11 @@
+const RootLayout = () => {
+  return (
+    <div>
+      <header></header>
+      <main></main>
+      <footer></footer>
+    </div>
+  );
+};
+
+export default RootLayout;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../Layouts/RootLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+  },
+]);
+
+export default router;
