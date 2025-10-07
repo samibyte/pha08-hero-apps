45301cc114feff0553939ee23798be03e109ad65
samibyte <samibyte.web@gmail.com>
2025-10-07 23:09:15 +0600

    Add footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,48 @@
+import { FaFacebook, FaLinkedin, FaYoutube } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import footerLogo from "../assets/logo.png";
+
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <footer className="footer sm:footer-horizontal bg-[#001931] text-neutral-content p-10">
+      <aside>
+        <img className="w-20" src={footerLogo} alt="" />
+        <p className="text-2xl font-semibold">Hero Apps Co.</p>
+        <p>Copyright &copy; 2025 Hero Apps. All rights reserved.</p>
+      </aside>
+      <nav>
+        <h6 className="footer-title">Company</h6>
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+        <a className="link link-hover">Jobs</a>
+        <a className="link link-hover">Press kit</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Legal</h6>
+        <a className="link link-hover">Terms of use</a>
+        <a className="link link-hover">Privacy policy</a>
+        <a className="link link-hover">Cookie policy</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Social</h6>
+        <div className="grid grid-flow-col gap-4">
+          <a>
+            <FaSquareXTwitter size={20} />
+          </a>
+
+          <a>
+            <FaLinkedin size={20} />
+          </a>
+          <a>
+            <FaFacebook size={20} />
+          </a>
+          <a>
+            <FaYoutube size={20} />
+          </a>
+        </div>
+      </nav>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -1,13 +1,19 @@
+import Footer from "../Components/Footer";
 import Navbar from "../Components/Navbar";
+import Home from "../Pages/Home";
 
 const RootLayout = () => {
   return (
     <>
-      <header>
-        <Navbar />
-      </header>
-      <main></main>
-      <footer></footer>
+      <div className="flex flex-col min-h-screen">
+        <header className="fixed top-0 w-full z-37">
+          <Navbar />
+        </header>
+        <main className="flex-1">
+          <Home />
+        </main>
+      </div>
+      <Footer />
     </>
   );
 };
