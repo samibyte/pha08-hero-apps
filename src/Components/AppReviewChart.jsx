6d05d7fa300965f001120f8ea51ed94ffa1f33c9
samibyte <samibyte.web@gmail.com>
2025-10-09 17:06:00 +0600

    Implement lazy load in rechart components

---
+++ src/Components/AppReviewChart.jsx
@@ -0,0 +1,70 @@
+/* import {
+  Bar,
+  BarChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts"; */
+
+import { Suspense, lazy } from "react";
+import LoadingAnimation from "./LoadingAnimation";
+
+// Lazy load Recharts components
+const BarChart = lazy(() =>
+  import("recharts").then((module) => ({ default: module.BarChart }))
+);
+const Bar = lazy(() =>
+  import("recharts").then((module) => ({ default: module.Bar }))
+);
+const XAxis = lazy(() =>
+  import("recharts").then((module) => ({ default: module.XAxis }))
+);
+const YAxis = lazy(() =>
+  import("recharts").then((module) => ({ default: module.YAxis }))
+);
+const Tooltip = lazy(() =>
+  import("recharts").then((module) => ({ default: module.Tooltip }))
+);
+const ResponsiveContainer = lazy(() =>
+  import("recharts").then((module) => ({ default: module.ResponsiveContainer }))
+);
+
+const AppReviewChart = ({ ratings }) => {
+  return (
+    <div className="h-88 border-b-1 border-[rgba(0,25,49,0.2)] pb-12 mb-10 ">
+      <h3 className="text-2xl font-semibold text-[#001931]">Ratings</h3>
+      <Suspense fallback={<LoadingAnimation />}>
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart
+            data={ratings}
+            layout="vertical"
+            margin={{ top: 24, left: 16, bottom: 24 }}
+            tabIndex={-1}
+          >
+            <XAxis
+              type="number"
+              axisLine={false}
+              tickLine={false}
+              tickMargin={24}
+              tick={{ fill: "#627382" }}
+            />
+            <YAxis
+              dataKey="name"
+              type="category"
+              reversed
+              axisLine={false}
+              tickLine={false}
+              tickMargin={16}
+              tick={{ fill: "#627382" }}
+            />
+            <Tooltip />
+            <Bar dataKey="count" fill="#ff8811" barSize={28} />
+          </BarChart>
+        </ResponsiveContainer>
+      </Suspense>
+    </div>
+  );
+};
+
+export default AppReviewChart;
--- src/Pages/AppDetails.jsx
+++ src/Pages/AppDetails.jsx
@@ -6,14 +6,7 @@ import reviewIcon from "../assets/icon-review.png";
 import { useEffect, useState } from "react";
 import { loadInstalledApp, updateInstalledList } from "../Utils/localStorage";
 import { toast } from "react-toastify";
-import {
-  Bar,
-  BarChart,
-  ResponsiveContainer,
-  Tooltip,
-  XAxis,
-  YAxis,
-} from "recharts";
+import AppReviewChart from "../Components/AppReviewChart";
 
 const AppDetails = () => {
   const { id } = useParams();
@@ -110,37 +103,7 @@ const AppDetails = () => {
         </div>
       </div>
 
-      {/* app review chart */}
-      <div className="h-88 border-b-1 border-[rgba(0,25,49,0.2)] pb-12 mb-10 ">
-        <h3 className="text-2xl font-semibold text-[#001931]">Ratings</h3>
-        <ResponsiveContainer width="100%" height="100%">
-          <BarChart
-            data={ratings}
-            layout="vertical"
-            margin={{ top: 24, left: 16, bottom: 24 }}
-            tabIndex={-1}
-          >
-            <XAxis
-              type="number"
-              axisLine={false}
-              tickLine={false}
-              tickMargin={24}
-              tick={{ fill: "#627382" }}
-            />
-            <YAxis
-              dataKey="name"
-              type="category"
-              reversed
-              axisLine={false}
-              tickLine={false}
-              tickMargin={16}
-              tick={{ fill: "#627382" }}
-            />
-            <Tooltip />
-            <Bar dataKey="count" fill="#ff8811" barSize={28} />
-          </BarChart>
-        </ResponsiveContainer>
-      </div>
+      <AppReviewChart ratings={ratings} />
 
       {/* descriptions */}
       <div>
